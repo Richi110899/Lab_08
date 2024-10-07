a14476ab44808f1eb44667c1738cc79741bb5237
Ricardo Antonio Gómez Sotomayor <ricardo.gomez@tecsup.edu.pe>
2024-10-06 19:30:59 -0500

    Creacion de la clase TaskViewModel

--- app/src/main/java/com/example/lab08/TaskDatabase.kt
+++ app/src/main/java/com/example/lab08/TaskDatabase.kt
@@ -3,7 +3,6 @@ package com.example.lab08
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-
 @Database(entities = [Task::class], version = 1)
 abstract class TaskDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
